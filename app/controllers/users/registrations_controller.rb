02a1599aeb37ea70d14db1946a0431246f118b65
Bernard Lomo <bernardlomo2@gmail.comm>
2017-08-26 18:22:48 +0000

    new edits

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,19 +17,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  def create
- if !verify_recaptcha
-    flash.delete :recaptcha_error
-    build_resource(sign_up_params)
-    resource.valid?
-    resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
-    clean_up_passwords(resource)
-    respond_with_navigational(resource) { render_with_scope :new }
-  else
-    flash.delete :recaptcha_error
-    super
-  end
-end
 
   # PUT /resource
   # def update
