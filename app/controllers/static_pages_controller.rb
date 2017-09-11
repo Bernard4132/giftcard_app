06de551c843cd659e554521a5c7a85c89827bd6a
Bernard Lomo <bernardlomo2@gmail.comm>
2017-09-11 13:52:02 +0000

    new edits

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
 def home
 	if signed_in?
-	  @userconfirmedcards = (current_user.bitcoins_marked_as_confirm.where(approved: false).all) - (current_user.bitcoins.where(declined: true).all)
+	  @userconfirmedcards = (current_user.bitcoins_marked_as_confirm.where(approved: false).where(terms: true).all) - (current_user.bitcoins.where(declined: true).all)
 	  @userapprovedcards = current_user.bitcoins.where(approved: true).all
 	  @userdeclinedcards = current_user.bitcoins.where(declined: true).all
 	end	
@@ -10,7 +10,7 @@ end
 
 def dashboard
 	if current_user.admin?
-	@yetapprovedcards = (Bitcoin.where(approved: false).all) - (Bitcoin.where(declined: true).all)
+	@yetapprovedcards = (Bitcoin.where(approved: false).where(terms: true).all) - (Bitcoin.where(declined: true).all)
 	@approvedcards = Bitcoin.where(approved: true).all
 	@declinedcards = Bitcoin.where(declined: true).all
     end
